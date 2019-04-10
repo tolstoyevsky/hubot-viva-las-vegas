f0da85d9d63393bbbd8d10b46598daf60cda51c9
BehindLoader <simonsuprun@gmail.com>
2019-04-10 12:01:33 +0300

    Fix bug in dailySickExtension

--- src/schedulers/dailySickExtension.js
+++ src/schedulers/dailySickExtension.js
@@ -10,16 +10,16 @@ const utils = require('./../utils')
  * @param {Robot} robot - Hubot instance.
  */
 module.exports = async (robot) => {
-  const allUsers = await routines.getAllUsers(robot)
+  const users = await routines.getAllUsers(robot)
   const tomorrow = moment().add(1, 'day')
 
-  for (const { user } of allUsers.filter(item => item.user.sick)) {
-    if (vars.GOOGLE_API && user.sick.eventId) {
-      utils.getEventFromCalendar(robot, user.sick.eventId)
-        .then(event => {
-          event.data.end = { date: tomorrow.format('YYYY-MM-DD') }
-          utils.updateEventFromCalendar(robot, user.sick.eventId, event.data)
-        })
-    }
+  if (!vars.GOOGLE_API) return
+
+  for (const user of users.filter(user => user.sick && user.sick.eventId)) {
+    utils.getEventFromCalendar(robot, user.sick.eventId)
+      .then(event => {
+        event.data.end = { date: tomorrow.format('YYYY-MM-DD') }
+        utils.updateEventFromCalendar(robot, user.sick.eventId, event.data)
+      })
   }
 }
