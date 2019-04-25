bb1b0be913328886a65dababdfa831f9fde0bb0f
BehindLoader <simonsuprun@gmail.com>
2019-04-25 14:57:33 +0300

    Remove days limit in work from home

--- src/routes/work-from-home/workFromHomeAgree.js
+++ src/routes/work-from-home/workFromHomeAgree.js
@@ -4,8 +4,6 @@ const moment = require('moment')
 const vars = require('../../vars')
 const utils = require('../../utils')
 
-const { Stack } = require('../../stack')
-
 module.exports = async msg => {
   const username = msg.message.user.name
   const state = await utils.getStateFromBrain(msg.robot, username)
@@ -13,23 +11,27 @@ module.exports = async msg => {
 
   if (state.n === vars.WAITING_CONFIRMATION_STATE) {
     if (answer === 'да, согласован') {
+      const { dateRequested } = state
+
       state.n = vars.INIT_STATE
-      const dayOfWorkFromHome = new Stack(state.dateOfWorkFromHome)
-      dayOfWorkFromHome.push(state.dateRequested)
-      state.dateOfWorkFromHome = dayOfWorkFromHome
-      state.dateRequested = ''
+
+      state.dateOfWorkFromHome = state.dateOfWorkFromHome || []
+      state.dateOfWorkFromHome.push({ date: dateRequested })
+      state.dateRequested = null
+
       if (vars.GOOGLE_API) {
-        const date = moment(`${dayOfWorkFromHome[1]}`, vars.DATE_FORMAT)
+        const date = moment(dateRequested, vars.CREATION_DATE_FORMAT)
         const startDay = date.format('YYYY-MM-DD')
         const endDay = date.add(1, 'days').format('YYYY-MM-DD')
         const user = await routines.findUserByName(msg.robot, username)
+        const event = state.dateOfWorkFromHome.find(item => item.date === dateRequested)
 
         utils.addEventToCalendar(msg.robot, startDay, endDay, user, vars.GOOGLE_EVENT_WORK_FROM_HOME)
-          .then(eventId => { user.vivaLasVegas.homeWorkEventId = eventId })
+          .then(eventId => { event.eventId = eventId })
 
-        msg.send(`Отлично. Я создал событие в календаре. Ты работаешь из дома ${dayOfWorkFromHome[1]}.`)
+        msg.send(`Отлично. Я создал событие в календаре. Ты работаешь из дома ${dateRequested}.`)
       } else {
-        msg.send(`Отлично. Ты работаешь из дома ${dayOfWorkFromHome[1]}.`)
+        msg.send(`Отлично. Ты работаешь из дома ${dateRequested}.`)
       }
     } else {
       state.n = vars.INIT_STATE
